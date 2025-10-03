2b19c18a72fbd4b8816e9f70c0f639b0a95cdfdf
viswanatha <psviswanatha@gmail.com>
2025-10-04 02:06:43 +0530

    Bump version to 0.4.5

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
 
 setup(
     name='aird',
-    version="0.4.4",
+    version="0.4.5",
     packages=find_packages(),
     package_data={'aird': ['templates/*.html']},
     rust_extensions=rust_extensions,
@@ -59,6 +59,8 @@ setup(
     python_requires='>=3.10',
     zip_safe=False,  # Required for Rust extensions
 )
+
+
 
 
 
